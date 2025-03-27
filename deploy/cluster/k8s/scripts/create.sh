3ba3135eb03bbbcb557f9adfa1f98688f25fa291
Isaac Hunter <ikehunter5@gmail.com>
2025-03-27 12:45:48 -0400

    reconfigure k8s for local testing

--- deploy/cluster/k8s/scripts/create.sh
+++ deploy/cluster/k8s/scripts/create.sh
@@ -18,6 +18,11 @@ helm install redis bitnami/redis \
   --values=./charts/redis-values.yml \
   --namespace redis \
   --create-namespace
+  
+helm install postgres bitnami/postgresql \
+  --values=./charts/postgresql-values.yml \
+  --namespace postgres \
+  --create-namespace
 
 # Create main namespace if it doesn't exist
 kubectl create namespace main --dry-run=client -o yaml | kubectl apply -f -
