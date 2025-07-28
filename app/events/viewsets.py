89768278ce666fbd6de6a5293e7398141445b1d9
Isaac Hunter <ikehunter5@gmail.com>
2025-07-27 22:30:29 -0400

    implement caching for api responses, gzip for nginx, optimize querysets

--- app/events/viewsets.py
+++ app/events/viewsets.py
@@ -11,7 +11,7 @@ from . import models, serializers
 class EventViewset(ModelViewSetBase):
     """CRUD Api routes for Event models."""
 
-    queryset = models.Event.objects.all()
+    queryset = models.Event.objects.all().prefetch_related("hosts", "hosts__club", "tags")
     serializer_class = serializers.EventSerializer
 
     def check_object_permissions(self, request, obj):
