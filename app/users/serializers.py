b61d6e2ab0b757a821a346ac4c91332ca706b3b3
Jackie <jackieec956@gmail.com>
2025-08-14 17:09:01 -0400

    added socials to user serializer

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -40,6 +40,10 @@ class ProfileNestedSerializer(ModelSerializerBase):
         model = Profile
         exclude = ["user", "created_at", "updated_at"]
 
+class SocialProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialProfile
+        fields = ("id", "social_type", "url", "username", "order")
 
 class UserSerializer(ModelSerializer):
     """Represents a person in the system who can authenticate."""
@@ -50,6 +54,8 @@ class UserSerializer(ModelSerializer):
         source="club_memberships", many=True, required=False
     )
     profile = ProfileNestedSerializer(required=False)
+    socials = SocialProfileSerializer(many=True, required=False)
+
     is_email_verified = serializers.BooleanField(read_only=True)
     can_authenticate = serializers.BooleanField(read_only=True)
     is_club_admin = serializers.BooleanField(read_only=True)
@@ -63,6 +69,7 @@ class UserSerializer(ModelSerializer):
             "password",
             "clubs",
             "profile",
+            "socials",
             "is_onboarded",
             "is_email_verified",
             "can_authenticate",
