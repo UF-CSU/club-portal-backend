578578790d45f17d4de5832c5ac2df3b7e991e93
Isaac Hunter <ikehunter5@gmail.com>
2026-04-01 07:54:41 -0400

    remove test email

--- app/clubs/apps.py
+++ app/clubs/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from lib.emails import send_html_mail
 
 
 class ClubConfig(AppConfig):
@@ -9,8 +8,4 @@ class ClubConfig(AppConfig):
     def ready(self) -> None:
         from . import signals  # noqa: F401
 
-        send_html_mail(
-            "Test email", ["user@example.com"], html_template="test-email.html"
-        )
-
-        return super().ready() 
+        return super().ready()
